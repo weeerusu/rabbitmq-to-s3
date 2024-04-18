d75f11877b6ef1bd3e0e77cc0e559c019f94fda1
Qinthara Andini Hananto <qinthara.andini@ui.ac.id>
2024-04-18 09:25:32 +0700

    Adding s3 bucket upload function

--- rabbitmqs3.js
+++ rabbitmqs3.js
@@ -1,39 +1,66 @@
-const amqp = require('amqplib/callback_api');
+const express = require("express");
+const app = express();
 const AWS = require('aws-sdk');
+const PORT = process.env.PORT || 5672;
 
-exports.handler = async (event) => {
-    const s3 = new AWS.S3();
-    const bucketName = '<Your_Bucket_Name>';
-    const fileName = '<Your_File_Name>';
+app.use(express.json());
 
-    amqp.connect('amqp://<RabbitMQ_Host>', function(error0, connection) {
-        if (error0) {
-            throw error0;
-        }
-        connection.createChannel(function(error1, channel) {
-            if (error1) {
-                throw error1;
-            }
+const amqp = require("amqplib");
+var channel, connection;
 
-            const queue = '<Your_Queue_Name>';
+const exchange_name = "[name]";
+const exchange_type = "[type]";
+const queue_name = "[queue]";
 
-            channel.consume(queue, function(msg) {
-                console.log(" [x] Received %s", msg.content.toString());
+connectToRabbitMQ();
+async function connectToRabbitMQ() {
+  try {
+    connection = await amqp.connect("amqp://[url:port]");
+    channel = await connection.createChannel();
 
-                const params = {
-                    Bucket: bucketName,
-                    Key: fileName,
-                    Body: msg.content.toString()
-                };
+    connectToQueue();
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-                s3.upload(params, function(s3Err, data) {
-                    if (s3Err) throw s3Err
-                    console.log(`File uploaded successfully at ${data.Location}`)
-                });
+async function connectToQueue() {
+  // https://amqp-node.github.io/amqplib/channel_api.html#channel_assertExchange
+  await channel.assertExchange(exchange_name, exchange_type, {
+    durable: false,
+  });
 
-            }, {
-                noAck: true
-            });
+  const s3 = new AWS.S3();
+  const bucketName = '[bucket name]';
+  const fileName = '[file name]'; // name up to you
+
+  const q = await channel.assertQueue(queue_name, { exclusive: true });
+
+  // binding the queue
+  const binding_key = "";
+  channel.bindQueue(q.queue, exchange_name, binding_key);
+
+  console.log("Consuming messages from queue: ", q.queue);
+  channel.consume(
+    q.queue,
+    (msg) => {
+      if (msg.content)
+        console.log("\nReceived message: ", msg.content.toString());
+        channel.ack(msg);
+
+        const params = {
+            Bucket: bucketName,
+            Key: fileName,
+            Body: msg.content.toString()
+        };
+    
+        s3.upload(params, function(s3Err, data) {
+            if (s3Err) throw s3Err
+            console.log(`File uploaded successfully at ${data.Location}`)
         });
-    });
-};
+    }
+  );
+
+}
+
+app.listen(PORT, () => console.log("Server running at port " + PORT));
